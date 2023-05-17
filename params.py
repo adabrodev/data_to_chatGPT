ae8a51beb3982e629b6cfd7ead9ecdc949938342
“adabrodev” <adabro.dev@gmail.com>
2023-05-17 10:08:09 +0200

    first commit

---
+++ params.py
@@ -0,0 +1,8 @@
+params = {
+    ## datasets
+    'tgt_dataset_name': 'data_to_LLM_git',
+
+    ## tables
+    'tgt_latest_lessons_analytics_table_name': 'tgt_latest_lesson_analytics', # analytics with Busines indicators 
+    'interpretation_KPI_table_name': 'interpretation_KPI', # table with ChatGPT responses
+}
